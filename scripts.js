f648f127ba82044553d895e15b59e0bb4b656545
sablin <sablin.irk@gmail.com>
2022-05-24 16:38:36 +0800

    Тестовое V1

---
+++ scripts.js
@@ -0,0 +1,63 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+ //маска телефона
+var phoneInput = document.querySelector('.phone');
+var maskOptions = {
+    mask: '+7(#00)000-00-00',
+    lazy: false,
+    definitions: {
+        '#': /[1,2,3,4,5,6,9]/
+      }
+} 
+var mask = new IMask(phoneInput, maskOptions);
+
+// маска email
+var emailInput = document.getElementById('email');
+var maskOptions2 = {    
+    mask:function (value) {
+                if(/^[a-z0-9_\.-]+$/.test(value))
+                    return true;
+                if(/^[a-z0-9_\.-]+@$/.test(value))
+                    return true;
+                if(/^[a-z0-9_\.-]+@[a-z0-9-]+$/.test(value))
+                    return true;
+                if(/^[a-z0-9_\.-]+@[a-z0-9-]+\.$/.test(value))
+                    return true;
+                if(/^[a-z0-9_\.-]+@[a-z0-9-]+\.[a-z]{1,4}$/.test(value))
+                    return true;
+                if(/^[a-z0-9_\.-]+@[a-z0-9-]+\.[a-z]{1,4}\.$/.test(value))
+                    return true;
+                if(/^[a-z0-9_\.-]+@[a-z0-9-]+\.[a-z]{1,4}\.[a-z]{1,4}$/.test(value))
+                    return true;
+                return false;
+                    },
+    lazy: false
+} 
+var mask2 = new IMask(emailInput, maskOptions2);
+
+
+// обработка отправки формы
+document.querySelector('.contact-form').addEventListener('submit', (e) =>{
+e.preventDefault();
+const name = document.querySelector('.name').value;
+const phone = document.querySelector('.phone').value;
+const city = document.querySelector('.city').value;
+const email = document.querySelector('.email').value;
+const comType = document.querySelector('.com-type').value;
+const textInfo = document.querySelector('.text-info').value;
+const subject = document.querySelector('.subject').value
+const formData = {
+    name, phone, city, email, comType, textInfo
+}
+
+
+fetch('/feedback', formData)
+    .then(res => {
+        alert('Все работает');
+        e.target.reset();
+    })
+    .catch( res => alert(res))
+})
+
+});
